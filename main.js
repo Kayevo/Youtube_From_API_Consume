de586e2852f9fc1b5d542dd61e75621eba37a1d5
Kayevo <kayevo@protonmail.com>
2022-02-28 23:52:23 -0300

    feature: add request to see all users on database

--- main.js
+++ main.js
@@ -5,68 +5,39 @@ class Credential {
   }
 }
 
-class User  extends Credential{
+class User extends Credential {
   constructor(_email, _password, _isAdminUser) {
     super(_email, _password);
     this.isAdminUser = _isAdminUser;
   }
 }
 
-//
-
-function getUserTable(_url, _email, _password) {
-  var userCredentials = {
-    email: '"' + _email + '"',
-    password: '"' + _password + '"',
-  };
-
+function getUserTable(_url, _method, _userCredentials) {
   let request = new XMLHttpRequest();
-  request.open("GET", _url, false);
-  request.send(JSON.stringify(userCredentials));
+
+  request.open(_method, _url, false);
+  request.send(JSON.stringify(_userCredentials));
 
   return request.responseText;
 }
 
-//
+function signupUser(_url, _method, _user) {
+  let request = new XMLHttpRequest();
 
-// function signupUser(_url, _method, _user) {
-//   let request = new XMLHttpRequest();
+  request.open(_method, _url, false);
+  request.send(JSON.stringify(_user));
 
-//   request.open(_method, _url, false);
-//   request.send(JSON.stringify(_user));
-
-//   return request.responseText;
-// }
-
-// function getUserTableTest(_url) {
-//   var userCredentials = {
-//     email: "testA",
-//     password: "passA",
-//     isAdminUser: true,
-//   };
-
-//   var userCredentials2 = new YouTubeCredential("_email", "_password");
-
-//   var request = new XMLHttpRequest();
-
-//   // request.responseType = "json";
-
-//   request.open("POST", _url, false);
-//   // request.setRequestHeader("Content-Type", "application/json");
-
-//   request.send(JSON.stringify(userCredentials2));
-
-//   return request.responseText;
-// }
+  return request.responseText;
+}
 
 function main() {
-  user = new User("email1", "pass1", true);
-  signupUser("http://127.0.0.1:5000/user/signup", "POST", user);
+  youtubeUser = new User("email1", "pass1", true);
+  signupUser("http://127.0.0.1:5000/user/signup", "POST", youtubeUser);
 
-  userCredentials = new Credential(user.email, user.password);
-  
-  // console.log(getUserTableTest("http://127.0.0.1:5000/user/signup"));
-  // console.log(getUserTable("http://127.0.0.1:5000/user/table", "test1", "pass1"));
+  userCredentials = new Credential(youtubeUser.email, youtubeUser.password);
+  console.log(
+    getUserTable("http://127.0.0.1:5000/user/table", "POST", userCredentials)
+  );
 }
 
 main();
